38c1b021e3afa15cd829d032c53355188d62c44c
chris alvizuris <chris.alvizuris@gmail.com>
2021-09-03 13:48:51 -0700

    Initial commit

---
+++ src/main/java/com/pokemon/pokemonapi/pokemon/Pokemon.java
@@ -0,0 +1,48 @@
+package com.pokemon.pokemonapi.pokemon;
+
+public class Pokemon {
+    private int id;
+    private String name;
+    private String type;
+
+    public Pokemon(){};
+
+    public Pokemon(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
