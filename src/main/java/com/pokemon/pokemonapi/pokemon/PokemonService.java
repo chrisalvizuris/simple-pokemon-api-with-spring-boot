ae726069ee1c655122f2cd56c7d716e5e102add1
chris alvizuris <chris.alvizuris@gmail.com>
2021-09-03 14:09:33 -0700

    updated pom file. Created Pokemon class. Created service and controller files.

---
+++ src/main/java/com/pokemon/pokemonapi/pokemon/PokemonService.java
@@ -0,0 +1,12 @@
+package com.pokemon.pokemonapi.pokemon;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PokemonService {
+    public List<Pokemon> getPokemon() {
+        return List.of(new Pokemon(1, "Pikachu", "Electric"));
+    }
+}
